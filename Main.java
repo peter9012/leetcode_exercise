1cebaaf750d5264bb1bc54a6f355cbacb329199c
Lu <peter.lu@US06321773.attlocal.net>
2021-01-23 14:41:39 -0800

    Add zigzag

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    decode_string_01 pa = new decode_string_01();
-    String str = "3[a]2[bc]";
-    System.out.println(pa.decodeString(str)); 
+    // decode_string_01 pa = new decode_string_01();
+    // String str = "3[a]2[bc]";
+    // System.out.println(pa.decodeString(str)); 
+
+    // zigzag_conversion zigzag_cov = new zigzag_conversion();
+    // String str = "PAYPALISHIRING";
+    // System.out.println(zigzag_cov.zigzagConversion(str, 3));
   }
 }
