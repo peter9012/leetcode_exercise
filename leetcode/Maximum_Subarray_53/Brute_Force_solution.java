334a452dfe5eb6ec79831d77672b66a6c7b533a0
peter9012 <rambo_lu@hotmail.com>
2021-12-01 01:21:25 -0800

    perfect

--- leetcode/Maximum_Subarray_53/Brute_Force_solution.java
+++ leetcode/Maximum_Subarray_53/Brute_Force_solution.java
@@ -16,10 +16,6 @@ public class Brute_Force_solution {
   public static void main(String[] args)
   {
     int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-//    f2
-    //f4
-    //f3
-    //f6
     System.out.println(maxSubArray(arr));
   }
 }
