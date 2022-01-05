4d3f6988f07677b42a8e4aedac86d63a8953dbdf
peter9012 <rambo_lu@hotmail.com>
2022-01-04 21:16:38 -0800

    reverse string

--- leetcode/Reverse_String_344/ReverseStringRecursion.java
+++ leetcode/Reverse_String_344/ReverseStringRecursion.java
@@ -11,4 +11,12 @@ public class ReverseStringRecursion {
   public void reverseString(char[] s) {
     helper(s, 0, s.length - 1);
   }
+//  public void reverseString(char[] s) {
+//    int left = 0, right = s.length - 1;
+//    while (left < right) {
+//      char tmp = s[left];
+//      s[left++] = s[right];
+//      s[right--] = tmp;
+//    }
+//  }
 }
