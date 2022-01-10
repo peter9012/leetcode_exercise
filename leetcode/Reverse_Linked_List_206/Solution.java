75c015e58f82a230a4f81dca25f3f196a37b9cd2
peter9012 <rambo_lu@hotmail.com>
2022-01-09 23:12:11 -0800

    reverse Linked List

---
+++ leetcode/Reverse_Linked_List_206/Solution.java
@@ -0,0 +1,11 @@
+package Reverse_Linked_List_206;
+
+public class Solution {
+  public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
+  }
+}
