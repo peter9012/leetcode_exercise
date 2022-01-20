f0e30821e135531712b63e7f9d124f00593fa7d6
peter9012 <rambo_lu@hotmail.com>
2022-01-19 22:26:33 -0800

    Sparse Matrix Multiplication 311

---
+++ leetcode/Spars_Matrix_Multiplication_311/Solution.java
@@ -0,0 +1,19 @@
+package Spars_Matrix_Multiplication_311;
+
+public class Solution {
+  public int[][] multiply(int[][] A, int[][] B) {
+    int m = A.length, n = A[0].length, nB = B[0].length;
+    int[][] C = new int[m][nB];
+
+    for(int i = 0; i < m; i++) {
+      for(int k = 0; k < n; k++) {
+        if (A[i][k] != 0) {
+          for (int j = 0; j < nB; j++) {
+            if (B[k][j] != 0) C[i][j] += A[i][k] * B[k][j];
+          }
+        }
+      }
+    }
+    return C;
+  }
+}
