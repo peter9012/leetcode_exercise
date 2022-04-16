299f41d770714872e68ade4d739235dc4a2b9f5a
peter9012 <rambo_lu@hotmail.com>
2022-04-16 17:06:25 +0800

    Decode_Ways_91

---
+++ leetcode/Decode_Ways_91/Solution.java
@@ -0,0 +1,50 @@
+package Decode_Ways_91;
+
+import java.util.*;
+
+public class Solution {
+
+  static Map<Integer, Integer> memo = new HashMap<>();
+
+  public static int numDecodings(String s) {
+    return recursiveWithMemo(0, s);
+  }
+
+  private static int recursiveWithMemo(int index, String str) {
+    // Have we already seen this substring?
+    if (memo.containsKey(index)) {
+      return memo.get(index);
+    }
+
+    // If you reach the end of the string
+    // Return 1 for success.
+    if (index == str.length()) {
+      return 1;
+    }
+
+    // If the string starts with a zero, it can't be decoded
+    if (str.charAt(index) == '0') {
+      return 0;
+    }
+
+    if (index == str.length() - 1) {
+      return 1;
+    }
+
+    int ans = recursiveWithMemo(index + 1, str);
+    if (Integer.parseInt(str.substring(index, index + 2)) <= 26) {
+      ans += recursiveWithMemo(index + 2, str);
+    }
+
+    // Save for memoization
+    memo.put(index, ans);
+
+    return ans;
+  }
+
+  public static void main(String[] args)
+  {
+    String str = "226";
+    System.out.printf("Count is %d", numDecodings(str));
+  }
+}
