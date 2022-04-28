9b04150c2f5bd4f272ad9a8b0e4c510716f2f654
peter9012 <rambo_lu@hotmail.com>
2022-04-28 16:03:09 +0800

    Maximum_Product_Subarray_152

---
+++ leetcode/Maximum_Product_Subarray_152/Solution.java
@@ -0,0 +1,29 @@
+package Maximum_Product_Subarray_152;
+
+public class Solution {
+  public static int maxProduct(int[] nums) {
+    if (nums.length == 0) return 0;
+
+    int max_so_far = nums[0];
+    int min_so_far = nums[0];
+    int result = max_so_far;
+
+    for (int i = 1; i < nums.length; i++) {
+      int curr = nums[i];
+      int temp_max = Math.max(curr, Math.max(max_so_far * curr, min_so_far * curr));
+      min_so_far = Math.min(curr, Math.min(max_so_far * curr, min_so_far * curr));
+
+      max_so_far = temp_max;
+
+      result = Math.max(max_so_far, result);
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args)
+  {
+    int[] arr = {2, 3, -2, 4};
+    System.out.println(maxProduct(arr));
+  }
+}
