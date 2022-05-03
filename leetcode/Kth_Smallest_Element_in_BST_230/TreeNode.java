05a6e211a31df0dc398025a4eebc6a76e2d5c42a
peter9012 <rambo_lu@hotmail.com>
2022-05-03 15:52:24 +0800

    Kth_Smallest_Element_in_BST_230

---
+++ leetcode/Kth_Smallest_Element_in_BST_230/TreeNode.java
@@ -0,0 +1,20 @@
+package Kth_Smallest_Element_in_BST_230;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
