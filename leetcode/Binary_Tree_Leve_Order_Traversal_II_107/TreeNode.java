91fd6e9b099f8c4b84bba38a741d7b6a05d673e7
peter9012 <rambo_lu@hotmail.com>
2022-09-03 23:47:25 -0700

    Binary_Tree_Leve_Order_Traversal_II_107

---
+++ leetcode/Binary_Tree_Leve_Order_Traversal_II_107/TreeNode.java
@@ -0,0 +1,20 @@
+package Binary_Tree_Leve_Order_Traversal_II_107;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
