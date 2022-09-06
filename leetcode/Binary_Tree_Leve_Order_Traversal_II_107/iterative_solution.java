be9d30dcde8cc2d8a74a58f65c95f07d54dee9ad
peter9012 <rambo_lu@hotmail.com>
2022-09-06 01:54:13 -0700

    /Binary_Tree_Leve_Order_Traversal_II_107

---
+++ leetcode/Binary_Tree_Leve_Order_Traversal_II_107/iterative_solution.java
@@ -0,0 +1,33 @@
+package Binary_Tree_Leve_Order_Traversal_II_107;
+
+import java.util.*;
+
+public class iterative_solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+      List<List<Integer>> levels = new ArrayList<List<Integer>>();
+      if (root == null) return levels;
+
+      ArrayDeque<TreeNode> nextLevel = new ArrayDeque() {{ offer(root); }};
+      ArrayDeque<TreeNode> currLevel = new ArrayDeque();
+
+      while (!nextLevel.isEmpty()) {
+        currLevel = nextLevel.clone();
+        nextLevel.clear();
+        levels.add(new ArrayList<Integer>());
+
+        for (TreeNode node : currLevel) {
+          // append the current node value
+          levels.get(levels.size() - 1).add(node.val);
+
+          // process child nodes for the next level
+          if (node.left != null)
+            nextLevel.offer(node.left);
+          if (node.right != null)
+            nextLevel.offer(node.right);
+        }
+      }
+
+      Collections.reverse(levels);
+      return levels;
+    }
+}
