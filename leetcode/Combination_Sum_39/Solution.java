24147fe6dc233a22f2d840e3f49bc4df62df13a0
peter9012 <rambo_lu@hotmail.com>
2022-09-10 00:58:56 -0700

    Combination_Sum_39

---
+++ leetcode/Combination_Sum_39/Solution.java
@@ -0,0 +1,46 @@
+package Combination_Sum_39;
+
+import java.util.*;
+
+class Solution {
+
+  protected static void backtrack(
+          int remain,
+          LinkedList<Integer> comb,
+          int start, int[] candidates,
+          List<List<Integer>> results) {
+
+    if (remain == 0) {
+      // make a deep copy of the current combination
+      results.add(new ArrayList<Integer>(comb));
+      return;
+    } else if (remain < 0) {
+      // exceed the scope, stop exploration.
+      return;
+    }
+
+    for (int i = start; i < candidates.length; ++i) {
+      // add the number into the combination
+      comb.add(candidates[i]);
+      backtrack(remain - candidates[i], comb, i, candidates, results);
+      // backtrack, remove the number from the combination
+      comb.removeLast();
+    }
+  }
+
+  public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+    List<List<Integer>> results = new ArrayList<List<Integer>>();
+    LinkedList<Integer> comb = new LinkedList<Integer>();
+
+    backtrack(target, comb, 0, candidates, results);
+    return results;
+  }
+
+  public static void main(String[] args)
+  {
+    int[] candidates = {2, 3, 5};
+    int target = 8;
+    System.out.println(combinationSum(candidates,target));
+  }
+}
+
