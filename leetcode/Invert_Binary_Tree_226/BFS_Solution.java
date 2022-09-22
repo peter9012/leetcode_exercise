f5a48729d1481690e29c47c7bf1a717da85e40f9
peter9012 <rambo_lu@hotmail.com>
2022-09-22 00:25:04 -0700

    Invert_Binary_Tree_226

--- leetcode/Invert_Binary_Tree_226/BFS_Solution.java
+++ leetcode/Invert_Binary_Tree_226/BFS_Solution.java
@@ -1,5 +1,27 @@
 package Invert_Binary_Tree_226;
 
-public class BFS_Solution {
+import java.util.*;
 
+public class BFS_Solution {
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) {return null;}
+    ArrayDeque<TreeNode> deque = new ArrayDeque<>();
+    deque.offer(root);
+    while (!deque.isEmpty()) {
+      int size = deque.size();
+      while (size-- > 0) {
+        TreeNode node = deque.poll();
+        swap(node);
+        if (node.left != null) {deque.offer(node.left);}
+        if (node.right != null) {deque.offer(node.right);}
+      }
+    }
+    return root;
+  }
+
+  public void swap(TreeNode root) {
+    TreeNode temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+  }
 }
