d1f91332692e13c9d3c00272fcda4f3c15a6d672
peter9012 <rambo_lu@hotmail.com>
2022-10-05 21:46:49 -0700

    Same_Tree_100

---
+++ leetcode/Same_Tree_100/Solution.java
@@ -0,0 +1,13 @@
+package Same_Tree_100;
+
+public class Solution {
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    // p and q are both null
+    if (p == null && q == null) return true;
+    // one of p and q is null
+    if (q == null || p == null) return false;
+    if (p.val != q.val) return false;
+    return isSameTree(p.right, q.right) &&
+            isSameTree(p.left, q.left);
+  }
+}
