ccf72a362187b9639e3d4a01819a717980c4ffed
peter9012 <rambo_lu@hotmail.com>
2022-10-10 00:03:47 -0700

    Find_Bottom_Left_Tree_Value_513

---
+++ leetcode/Find_Bottom_Left_Tree_Value_513/Solution.java
@@ -0,0 +1,49 @@
+package Find_Bottom_Left_Tree_Value_513;
+
+
+class Solution {
+  private static int Deep = -1;
+  private static int value = 0;
+  public static int findBottomLeftValue(TreeNode root) {
+    value = root.val;
+    findLeftValue(root,0);
+    return value;
+  }
+
+  private static void findLeftValue(TreeNode root, int deep) {
+    if (root == null) return;
+    if (root.left == null && root.right == null) {
+      if (deep > Deep) {
+        value = root.val;
+        Deep = deep;
+      }
+    }
+    if (root.left != null) findLeftValue(root.left,deep + 1);
+    if (root.right != null) findLeftValue(root.right,deep + 1);
+  }
+
+  public static void main(String[] args) {
+
+    /* Construct the following tree
+                   1
+                 /   \
+                /     \
+               2       3
+              /      /   \
+             /      /     \
+            4      5       6
+                  / \
+                 /   \
+                7     8
+    */
+    TreeNode tree = new TreeNode(1);
+    tree.left = new TreeNode(2);
+    tree.right = new TreeNode(3);
+    tree.left.left = new TreeNode(4);
+    tree.right.left = new TreeNode(5);
+    tree.right.right = new TreeNode(6);
+    tree.right.left.left = new TreeNode(7);
+    tree.right.left.right = new TreeNode(8);
+    System.out.println(findBottomLeftValue(tree));
+  }
+}
---
+++ leetcode/Find_Bottom_Left_Tree_Value_513/TreeNode.java
@@ -0,0 +1,20 @@
+package Find_Bottom_Left_Tree_Value_513;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
