37f8ce7cfe505efbce4b71b3b5f938cdc398818b
peter9012 <rambo_lu@hotmail.com>
2022-10-12 22:04:51 -0700

    Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal_106

---
+++ leetcode/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal_106/TreeNode.java
@@ -0,0 +1,20 @@
+package Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal_106;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
