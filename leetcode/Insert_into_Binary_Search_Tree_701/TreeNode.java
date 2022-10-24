98b36a8a9c15b289d7196835749209dc923200de
peter9012 <rambo_lu@hotmail.com>
2022-10-24 12:44:17 -0700

    Insert_into_Binary_Search_Tree_701

---
+++ leetcode/Insert_into_Binary_Search_Tree_701/TreeNode.java
@@ -0,0 +1,14 @@
+package Insert_into_Binary_Search_Tree_701;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode() {}
+  TreeNode(int val) { this.val = val; }
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
