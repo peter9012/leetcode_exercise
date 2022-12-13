5845f7d7881eeeb88e9e36073dd7ac0b952bae69
peter9012 <rambo_lu@hotmail.com>
2022-12-12 22:27:44 -0800

    Jump_Game_55

---
+++ leetcode/Jump_Game_55/Solution.java
@@ -0,0 +1,24 @@
+package Jump_Game_55;
+
+public class Solution {
+  public static boolean canJump(int[] nums) {
+    if (nums.length == 1) {
+      return true;
+    }
+    //覆盖范围, 初始覆盖范围应该是0，因为下面的迭代是从下标0开始的
+    int coverRange = 0;
+    //在覆盖范围内更新最大的覆盖范围
+    for (int i = 0; i <= coverRange; i++) {
+      coverRange = Math.max(coverRange, i + nums[i]);
+      if (coverRange >= nums.length - 1) {
+        return true;
+      }
+    }
+    return false;
+  }
+  public static void main(String args[])
+  {
+    int[] nums = {2,3,1,1,4};
+    System.out.println(canJump(nums));
+  }
+}
