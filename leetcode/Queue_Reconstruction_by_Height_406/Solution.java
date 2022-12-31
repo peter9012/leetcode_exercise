7d44ba27ae5f3f211ad584e53c3dc40eac895813
peter9012 <rambo_lu@hotmail.com>
2022-12-30 23:58:32 -0800

    Queue_Reconstruction_by_Height_406

--- leetcode/Queue_Reconstruction_by_Height_406/Solution.java
+++ leetcode/Queue_Reconstruction_by_Height_406/Solution.java
@@ -3,7 +3,7 @@ package Queue_Reconstruction_by_Height_406;
 import java.util.*;
 
 public class Solution {
-  public int[][] reconstructQueue(int[][] people) {
+  public static int[][] reconstructQueue(int[][] people) {
     // 身高从大到小排（身高相同k小的站前面）
     Arrays.sort(people, (a, b) -> {
       if (a[0] == b[0]) return a[1] - b[1];
@@ -17,5 +17,11 @@ public class Solution {
     }
 
     return que.toArray(new int[people.length][]);
+  }
+  public static void main(String[] args)
+  {
+    int[][] people = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+    int[][] output = reconstructQueue(people);
+    System.out.println(Arrays.deepToString(output));
   }
 }
