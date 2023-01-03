fbf861e2d24984dd6f7b2a2d47c9b34a09d5be37
peter9012 <rambo_lu@hotmail.com>
2023-01-03 14:52:06 -0800

    Partition_Labels_763

---
+++ leetcode/Partition_Labels_763/Solution.java
@@ -0,0 +1,24 @@
+package Partition_Labels_763;
+
+import java.util.*;
+
+public class Solution {
+  public List<Integer> partitionLabels(String S) {
+    List<Integer> list = new LinkedList<>();
+    int[] edge = new int[26];
+    char[] chars = S.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      edge[chars[i] - 'a'] = i;
+    }
+    int idx = 0;
+    int last = -1;
+    for (int i = 0; i < chars.length; i++) {
+      idx = Math.max(idx,edge[chars[i] - 'a']);
+      if (i == idx) {
+        list.add(i - last);
+        last = i;
+      }
+    }
+    return list;
+  }
+}
