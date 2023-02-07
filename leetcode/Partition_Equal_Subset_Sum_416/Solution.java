781441bc126109360a3e17068ad9604edac4815b
peter9012 <rambo_lu@hotmail.com>
2023-02-07 15:12:35 -0800

    Partition_Equal_Subset_Sum_416

---
+++ leetcode/Partition_Equal_Subset_Sum_416/Solution.java
@@ -0,0 +1,23 @@
+package Partition_Equal_Subset_Sum_416;
+
+public class Solution {
+  public boolean canPartition(int[] nums) {
+    if(nums == null || nums.length == 0) return false;
+    int n = nums.length;
+    int sum = 0;
+    for(int num : nums) {
+      sum += num;
+    }
+    //总和为奇数，不能平分
+    if(sum % 2 != 0) return false;
+    int target = sum / 2;
+    int[] dp = new int[target + 1];
+    for(int i = 0; i < n; i++) {
+      for(int j = target; j >= nums[i]; j--) {
+        //物品 i 的重量是 nums[i]，其价值也是 nums[i]
+        dp[j] = Math.max(dp[j], dp[j-nums[i]] + nums[i]);
+      }
+    }
+    return dp[target] == target;
+  }
+}
