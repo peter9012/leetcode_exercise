3f2d99aae2bedad3ec6b96cbfd7f771e57463b30
peter9012 <rambo_lu@hotmail.com>
2023-02-25 22:22:21 -0800

    Zigzag_Conversion_06

--- leetcode/Zigzag_Conversion_06/Solution.java
+++ leetcode/Zigzag_Conversion_06/Solution.java
@@ -1,5 +1,6 @@
+package Zigzag_Conversion_06;
 
-public class zigzag_conversion {
+public class Solution {
   public String zigzagConversion(String s, int numRows) {
 
     // if (numRows == 1) return s;
@@ -16,11 +17,11 @@ public class zigzag_conversion {
     //   if (curRow == 0 || curRow == numRows -1) goingDown = !goingDown;
     //   curRow += goingDown ? 1: -1;
     // }
-    
+
     // StringBuilder ret = new StringBuilder();
     // for (StringBuilder row : rows) ret.append(row);
     // return ret.toString();
-    
+
 
 
     if (numRows == 1) return s;
@@ -30,13 +31,12 @@ public class zigzag_conversion {
     int cycleLen = 2 * numRows - 2;
 
     for (int i = 0; i < numRows; i++) {
-        for (int j = 0; j + i < n; j += cycleLen) {
-            ret.append(s.charAt(j + i));
-            if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
-                ret.append(s.charAt(j + cycleLen - i));
-        }
+      for (int j = 0; j + i < n; j += cycleLen) {
+        ret.append(s.charAt(j + i));
+        if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+          ret.append(s.charAt(j + cycleLen - i));
+      }
     }
     return ret.toString();
   }
-  
 }
