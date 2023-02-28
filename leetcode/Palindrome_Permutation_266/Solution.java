e50c21bf8dd123209fea21c1c19e6d2a1e933fdf
peter9012 <rambo_lu@hotmail.com>
2023-02-28 14:03:45 -0800

    Palindrome_Permutation_266

---
+++ leetcode/Palindrome_Permutation_266/Solution.java
@@ -0,0 +1,17 @@
+package Palindrome_Permutation_266;
+
+import java.util.*;
+
+public class Solution {
+  boolean canPermutePalindrome(String s) {
+    HashMap< Character, Integer > map = new HashMap < > ();
+    for (int i = 0; i < s.length(); i++) {
+      map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+    }
+    int count = 0;
+    for (char key: map.keySet()) {
+      count += map.get(key) % 2;
+    }
+    return count <= 1;
+  }
+}
