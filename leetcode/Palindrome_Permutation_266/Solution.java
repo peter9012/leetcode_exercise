e262d9730bde1d71e219a4e71b94d8532ed647d9
peter9012 <rambo_lu@hotmail.com>
2023-02-28 16:49:15 -0800

    Palindrome_Permutation_266

--- leetcode/Palindrome_Permutation_266/Solution.java
+++ leetcode/Palindrome_Permutation_266/Solution.java
@@ -3,7 +3,7 @@ package Palindrome_Permutation_266;
 import java.util.*;
 
 public class Solution {
-  boolean canPermutePalindrome(String s) {
+  static boolean canPermutePalindrome(String s) {
     HashMap< Character, Integer > map = new HashMap < > ();
     for (int i = 0; i < s.length(); i++) {
       map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
@@ -13,5 +13,20 @@ public class Solution {
       count += map.get(key) % 2;
     }
     return count <= 1;
+  }
+  public static void main(String[] args)
+  {
+    String input = "code";
+    System.out.println(
+            "Given a string " + input
+            + " check permutation of the string could form a palindrome "
+            + canPermutePalindrome(input)
+    );
+    String input02 = "aab";
+    System.out.println(
+            "Given a string " + input02
+                    + " check permutation of the string could form a palindrome "
+                    + canPermutePalindrome(input02)
+    );
   }
 }
