dbb093ce1b84359fb10b193e9f7046e941bc02bf
peter9012 <rambo_lu@hotmail.com>
2023-03-17 21:48:29 -0700

    Remove_Duplicates_from_Sorted_Array_26

---
+++ leetcode/Remove_Duplicates_from_Sorted_Array_26/Solution.java
@@ -0,0 +1,22 @@
+package Remove_Duplicates_from_Sorted_Array_26;
+
+public class Solution {
+  public static int removeDuplicates(int[] nums) {
+    int insertIndex = 1;
+    for(int i = 1; i < nums.length; i++){
+      // We skip to next index if we see a duplicate element
+      if(nums[i - 1] != nums[i]) {
+                /* Storing the unique element at insertIndex index and incrementing
+                   the insertIndex by 1 */
+        nums[insertIndex] = nums[i];
+        insertIndex++;
+      }
+    }
+    return insertIndex;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {0,0,1,1,1,2,2,3,3,4};
+    System.out.println(removeDuplicates(nums));
+  }
+}
