36457c0a350eb5059e1d5fb36ad6998ad8e4dfff
peter9012 <rambo_lu@hotmail.com>
2023-03-18 23:48:19 -0700

    minor change

--- leetcode/Remove_Duplicates_from_Sorted_Array_26/Solution.java
+++ leetcode/Remove_Duplicates_from_Sorted_Array_26/Solution.java
@@ -14,7 +14,6 @@ public class Solution {
     }
     return insertIndex;
   }
-
   public static void main(String[] args) {
     int[] nums = {0,0,1,1,1,2,2,3,3,4};
     System.out.println(removeDuplicates(nums));
