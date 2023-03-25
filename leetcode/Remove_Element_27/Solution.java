052cf01cf5d50f06767bcd36b745a492255435a9
peter9012 <rambo_lu@hotmail.com>
2023-03-24 19:02:40 -0700

    Remove_Element_27

--- leetcode/Remove_Element_27/Solution.java
+++ leetcode/Remove_Element_27/Solution.java
@@ -1,5 +1,6 @@
-public class remove_element {
-  // Time complexity : O(n) Space complexity : O(1) 
+package Remove_Element_27;
+
+public class Solution {
   // Approach 1: Two Pointers
   // public int removeElement(int[] nums, int val) {
   //   int i = 0;
@@ -14,7 +15,7 @@ public class remove_element {
 
   // Approach 2: Two Pointers - when elements to remove are rare
   // When we encounter nums[i] = valnums[i]=val, we can swap the current element out with the last element and dispose the last one. This essentially reduces the array's size by 1.
-  public int removeElement(int[] nums, int val) {
+  public static int removeElement(int[] nums, int val) {
     int i = 0;
     int n = nums.length;
     while (i < n) {
@@ -26,5 +27,10 @@ public class remove_element {
       }
     }
     return n;
+  }
+  public static void main(String[] args) {
+    int[] nums = new int[]{0,1,2,2,3,0,4,2};
+    int val = 2;
+    System.out.println(removeElement(nums, 2));
   }
 }
