ea7e5666e36a7145525e70e905117b9d80a5b43c
peter9012 <rambo_lu@hotmail.com>
2023-03-27 23:47:52 -0700

    Remove_Duplicates_from_Sorted_List_83

---
+++ leetcode/Remove_Duplicates_from_Sorted_List_83/Solution.java
@@ -0,0 +1,20 @@
+package Remove_Duplicates_from_Sorted_List_83;
+
+public class Solution {
+  public ListNode deleteDuplicates(ListNode head) {
+    ListNode list = head;
+
+    while(list != null) {
+      if (list.next == null) {
+        break;
+      }
+      if (list.val == list.next.val) {
+        list.next = list.next.next;
+      } else {
+        list = list.next;
+      }
+    }
+
+    return head;
+  }
+}
