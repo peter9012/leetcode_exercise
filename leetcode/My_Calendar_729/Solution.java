0071ae7d8d1ea01d4c90abc41706f9576855b3f8
peter9012 <rambo_lu@hotmail.com>
2023-04-11 22:51:56 -0700

    My_Calendar_729

---
+++ leetcode/My_Calendar_729/Solution.java
@@ -0,0 +1,24 @@
+package My_Calendar_729;
+
+import java.util.TreeMap;
+
+public class Solution {
+  class MyCalendar {
+    TreeMap<Integer, Integer> calendar;
+
+    MyCalendar() {
+      calendar = new TreeMap();
+    }
+
+    public boolean book(int start, int end) {
+      Integer prev = calendar.floorKey(start),
+              next = calendar.ceilingKey(start);
+      if ((prev == null || calendar.get(prev) <= start) &&
+              (next == null || end <= next)) {
+        calendar.put(start, end);
+        return true;
+      }
+      return false;
+    }
+  }
+}
