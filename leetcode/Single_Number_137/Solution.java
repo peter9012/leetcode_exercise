9fe91a4c6b635951d7a7cc31cc7fe77cff4c3232
peter9012 <rambo_lu@hotmail.com>
2023-04-26 23:46:12 -0700

    single number

--- leetcode/Single_Number_137/Solution.java
+++ leetcode/Single_Number_137/Solution.java
@@ -3,7 +3,7 @@ package Single_Number_137;
 import java.util.*;
 
 class Solution {
-  public int singleNumber(int[] nums) {
+  public static int singleNumber(int[] nums) {
     Set<Long> set = new HashSet<>();
     long sumSet = 0, sumArray = 0;
     for(int n : nums) {
@@ -12,5 +12,10 @@ class Solution {
     }
     for(Long s : set) sumSet += s;
     return (int)((3 * sumSet - sumArray) / 2);
+  }
+
+  public static void main(String[] args) {
+    int[] n = {2,2,3,2};
+    System.out.println(singleNumber(n));
   }
 }
