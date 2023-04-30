f6ad7f32e8e2078f1886b40e8055ae8dee104366
peter9012 <rambo_lu@hotmail.com>
2023-04-29 22:44:35 -0700

    Odd_Even_Linked_List_328

--- leetcode/Odd_Even_Linked_List_328/Solution.java
+++ leetcode/Odd_Even_Linked_List_328/Solution.java
@@ -1,7 +1,7 @@
 package Odd_Even_Linked_List_328;
 
 public class Solution {
-  public ListNode oddEvenList(ListNode head) {
+  public static ListNode oddEvenList(ListNode head) {
     if (head == null) return null;
     ListNode odd = head, even = head.next, evenHead = even;
     while (even != null && even.next != null) {
@@ -14,4 +14,24 @@ public class Solution {
     return head;
   }
 
+  public static void printList(ListNode head)
+  {
+		ListNode ptr = head;
+      while (ptr != null)
+      {
+          System.out.print(ptr.val + " —> ");
+          ptr = ptr.next;
+      }
+      System.out.println("null");
+  }
+
+  public static void main(String[] args) {
+    ListNode list1 = new ListNode(1);
+    list1.next = new ListNode(2);
+    list1.next.next = new ListNode(3);
+    list1.next.next.next = new ListNode(4);
+    list1.next.next.next.next = new ListNode(5);
+    printList(list1);
+    printList(oddEvenList(list1));
+  }
 }
