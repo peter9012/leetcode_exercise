7568b431492e2003fae9645b1352e76fb0074b44
peter9012 <rambo_lu@hotmail.com>
2023-05-15 22:35:15 -0700

    Binary_Search_Tree_Iterator_173

---
+++ leetcode/Binary_Search_Tree_Iterator_173/TreeNode.java
@@ -0,0 +1,20 @@
+package Binary_Search_Tree_Iterator_173;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
