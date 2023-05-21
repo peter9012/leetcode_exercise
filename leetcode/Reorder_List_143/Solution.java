96072de0d6653862dc4cb4debef5784e17147cc1
peter9012 <rambo_lu@hotmail.com>
2023-05-20 19:13:23 -0700

    Reorder_List_143

---
+++ leetcode/Reorder_List_143/Solution.java
@@ -0,0 +1,54 @@
+package Reorder_List_143;
+
+public class Solution {
+  public static ListNode reorderList(ListNode head) {
+    if (head == null) return head;
+    ListNode slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    ListNode prev = null, curr = slow, tmp;
+    while(curr != null) {
+      tmp = curr.next;
+
+      curr.next = prev;
+      prev = curr;
+      curr = tmp;
+    }
+
+    ListNode first = head, second = prev;
+    while (second.next != null) {
+      tmp = first.next;
+      first.next = second;
+      first = tmp;
+
+      tmp = second.next;
+      second.next = first;
+      second = tmp;
+    }
+    return head;
+  }
+
+  public static void printList(ListNode head)
+  {
+		ListNode ptr = head;
+      while (ptr != null)
+      {
+          System.out.print(ptr.val + " —> ");
+          ptr = ptr.next;
+      }
+      System.out.println("null");
+  }
+
+  public static void main(String[] args) {
+    int[] keys = {1, 2, 3, 4, 5, 6};
+    ListNode l1 = null;
+    for (int i = keys.length - 1; i >= 0; i--) {
+      l1 = new ListNode(keys[i],l1);
+    }
+    printList(l1);
+    printList(reorderList(l1));
+  }
+}
