ed807ee962ae1d82140f80508889c2cfab993743
peter9012 <rambo_lu@hotmail.com>
2023-05-24 23:03:59 -0700

    Contains_Duplicate_II_219

---
+++ leetcode/Contains_Duplicate_II_219/Solution.java
@@ -0,0 +1,17 @@
+package Contains_Duplicate_II_219;
+
+import java.util.*;
+
+public class Solution {
+  public boolean containsNearbyDuplicate(int[] nums, int k) {
+    Set<Integer> set = new HashSet<>();
+    for (int i = 0; i < nums.length; ++i) {
+      if (set.contains(nums[i])) return true;
+      set.add(nums[i]);
+      if (set.size() > k) {
+        set.remove(nums[i - k]);
+      }
+    }
+    return false;
+  }
+}
