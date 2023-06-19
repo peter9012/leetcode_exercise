0e4fe9aed14a57542693f7b968c9a8b4ca9ccae9
peter9012 <rambo_lu@hotmail.com>
2023-06-18 17:30:47 -0700

    minor change

--- leetcode/Palindrome_Permutation_II/solution.java
+++ leetcode/Palindrome_Permutation_II/solution.java
@@ -8,6 +8,7 @@ public class solution {
     permute(s.toCharArray(), 0);
     return new ArrayList < String > (set);
   }
+  
   public boolean isPalindrome(char[] s) {
     for (int i = 0; i < s.length; i++) {
       if (s[i] != s[s.length - 1 - i])
@@ -15,11 +16,14 @@ public class solution {
     }
     return true;
   }
+
   public void swap(char[] s, int i, int j) {
     char temp = s[i];
     s[i] = s[j];
     s[j] = temp;
   }
+
+
   void permute(char[] s, int l) {
     if (l == s.length) {
       if (isPalindrome(s))
