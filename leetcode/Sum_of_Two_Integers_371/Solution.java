febc8588723e433463dd7a2beecfc68bf9efb233
peter9012 <rambo_lu@hotmail.com>
2023-06-27 16:07:23 -0700

    Sum_of_Two_Integers_371

--- leetcode/Sum_of_Two_Integers_371/Solution.java
+++ leetcode/Sum_of_Two_Integers_371/Solution.java
@@ -1,7 +1,8 @@
 package Sum_of_Two_Integers_371;
 
 public class Solution {
-  public int getSum(int a, int b) {
+  public static int getSum(int a, int b) {
+
     if (a == 0) return b;
     if (b == 0) return a;
 
@@ -10,7 +11,12 @@ public class Solution {
       a = a ^ b;
       b = carry << 1;
     }
-
     return a;
+  }
+
+  public static void main(String[] args)
+  {
+    int a = 19, b = 1;
+    System.out.println(getSum(a,b));
   }
 }
