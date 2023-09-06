a3270019736d98dd691c8cd2366ac452f8396f59
peter9012 <rambo_lu@hotmail.com>
2023-09-05 23:20:44 -0700

    peakIndexInMountainArray

---
+++ leetcode/Peak_Index_Mountain_Array_852/Solution02.java
@@ -0,0 +1,15 @@
+package Peak_Index_Mountain_Array_852;
+
+public class Solution02 {
+  public int peakIndexInMountainArray(int[] arr) {
+    int l = 0, r = arr.length - 1, mid;
+    while (l < r) {
+      mid = (l + r) / 2;
+      if (arr[mid] < arr[mid + 1])
+        l = mid + 1;
+      else
+        r = mid;
+    }
+    return l;
+  }
+}
